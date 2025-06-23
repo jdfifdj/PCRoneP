4a7ee54ac17a5bd93395de0958b63f85c59fe746
faiz555 <1837757414@qq.com>
2025-06-23 16:50:21 +0800

    工会战简单set推荐网站

--- ghz-config.js
+++ ghz-config.js
@@ -1,5 +1,22 @@
+// config.js
+const GHZconfigData = {
 
-new Vue({
-  el: '#app',
-  render: h => h(PCRoneGHZ)
-});
+        tabs: ['D1', 'D2', 'D3', 'D4', 'D5'],
+        activeTab: 0,
+        // 这里需要替换成实际的图片路径
+        d1Images: ['./工会战/d1-1.jpg', './工会战/d1-2.png'],
+        d2Images: ['./工会战/d2-1.jpg'],
+        d3Images: ['./工会战/d3-1.png','./工会战/d3-2.png','./工会战/d3-3.png',],
+        d4Images: ['./工会战/d4-1.jpg','./工会战/d4-2.png',],
+        d5Images: ['./工会战/d5-1.jpg','./工会战/d5-1a.jpg','./工会战/d5-2.jpg','./工会战/d5-2a.jpg',],
+        showBigImg: false,
+        bigImgSrc: ''
+
+}
+
+// 导出配置数据
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = GHZconfigData;
+} else {
+    window.configData = GHZconfigData;
+}
