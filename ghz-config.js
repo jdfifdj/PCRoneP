fe6034052b1898c57b3a88294887a1ca3710933c
faiz555 <1837757414@qq.com>
2025-06-24 12:22:07 +0800

    修复D3-3错误添加D2两刀

--- ghz-config.js
+++ ghz-config.js
@@ -5,7 +5,7 @@ const GHZconfigData = {
         activeTab: 0,
         // 这里需要替换成实际的图片路径
         d1Images: ['./工会战/d1-1.jpg', './工会战/d1-2.png'],
-        d2Images: ['./工会战/d2-1.jpg'],
+        d2Images: ['./工会战/d2-1.jpg','./工会战/d2-2.jpg','./工会战/d2-2a.jpg'],
         d3Images: ['./工会战/d3-1.png','./工会战/d3-2.png','./工会战/d3-3.png',],
         d4Images: ['./工会战/d4-1.jpg','./工会战/d4-2.png',],
         d5Images: ['./工会战/d5-1.jpg','./工会战/d5-1a.jpg','./工会战/d5-2.jpg','./工会战/d5-2a.jpg',],
