0a4591842bf524b441d38092ba3df06f8e7b74f7
faiz555 <1837757414@qq.com>
2025-06-25 10:35:57 +0800

    添加工会战简单set分刀和来源

--- ghz-config.js
+++ ghz-config.js
@@ -1,7 +1,7 @@
 // config.js
 const GHZconfigData = {
 
-        tabs: ['D1', 'D2', 'D3', 'D4', 'D5'],
+        tabs: ['D1', 'D2', 'D3', 'D4', 'D5','简单分刀'],
         activeTab: 0,
         // 这里需要替换成实际的图片路径
         d1Images: ['./工会战/d1-1.jpg', './工会战/d1-2.png'],
@@ -9,6 +9,7 @@ const GHZconfigData = {
         d3Images: ['./工会战/d3-1.png','./工会战/d3-2.png','./工会战/d3-3.png',],
         d4Images: ['./工会战/d4-1.jpg','./工会战/d4-2.png',],
         d5Images: ['./工会战/d5-1.jpg','./工会战/d5-1a.jpg','./工会战/d5-2.jpg','./工会战/d5-2a.jpg',],
+        d6Images: ['./工会战/花舞品级.png','./工会战/玩具喵abc-1.png','./工会战/玩具喵d-1.png','./工会战/花舞d-1.png','./工会战/花舞d-2.png','./工会战/花舞d-3.png','./工会战/花舞d-4.png','./工会战/花舞d-5.png'],
         showBigImg: false,
         bigImgSrc: ''
 
