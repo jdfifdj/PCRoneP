9f4da4b3ede22acf57981c1ef2a9325261d7c7be
faiz555 <1837757414@qq.com>
2025-09-20 17:12:21 +0800

    bot_help.html

---
+++ bothelp.js
@@ -0,0 +1,353 @@
+// config.js
+const configData = {
+  "currentTime": "加载中...",
+  "highlightKeywords": [
+    {
+      "keyword": "不推荐",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "不建议补",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "专武",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "开专建议：",
+      "className": "highlight-blue"
+    },
+    {
+      "keyword": "抽卡建议：",
+      "className": "highlight-blue"
+    },
+    {
+      "keyword": "刷取建议：",
+      "className": "highlight-blue"
+    },
+    {
+      "keyword": "等攻略",
+      "className": "highlight-blue"
+    },
+    {
+      "keyword": "「佩可莉姆（圣诞节）」：20号开始免费十连，钻>18w，免费十连没出货可补圣吃。",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「克蕾琪塔（圣诞节）」：圣吃180以上出的可以补。",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "年度垫底角色之一，仓管。",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "需要在开",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "必开，推荐交球",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "以下建议请结合自己的实际钻石来决定，请优先确保接下来10月份的重要限定能拿下。",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "有钻建议补",
+      "className": "highlight-blue"
+    },
+    {
+      "keyword": "（9w+）",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "（15w+）",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "（18w+）",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "但终会下岗（抽11月的花仓唯就行），没有的可以忍到她下岗，钻很多再考虑补",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "必开",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "必抽",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「初音（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「香澄（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "有强度但出场率不算高，连同春星抽很划算，老玩家建议无脑抽，除非抽0井1春初音（没钻抽到「初音（新年）」就停手）。",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "不抽这个池子",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「雪菲（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「佩可莉姆（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「似似花（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「伊莉亚（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「静流（情人节）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「可可萝（礼服）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「帆稀（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「美里（新年）」",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "钻多才建议补",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "目前可以不补",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "（15w+）",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "（6w+）",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "不用刻意补，补春龙妈的话看抽数（>150）补。",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "默认满专",
+      "className": "highlight-orange"
+    },
+    {
+      "keyword": "不建议抽",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「望（解放者）」：",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "「矛依未（解放者）」：",
+      "className": "highlight-red"
+    },
+    {
+      "keyword": "抽新不抽旧，仅钻多可考虑补票",
+      "className": "highlight-red"
+    }
+  ],
+  "gachaPools": [
+    {
+      "name": "n2或N2",
+      "start": "2025/08/11 11:00",
+      "end": "2025/08/14 10:59",
+      "shouldShow": true,
+      "advice": "刷本推荐,同理可以输入n3 n5 h2 h5之类",
+      "image": ""
+    },
+    {
+      "name": "活动",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "剧情活动攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "6星或六星",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "未来六星图",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "2专,二专,专二,专2",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "未来需要二专角色",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "千里眼",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "未来千里眼",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "作业",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "工会战出刀网",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "国rank,国r",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "国服rank推荐",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "一图流",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "公主连结未来半个月的活动，抽卡攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "抽卡",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "未来抽卡推荐",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "ex5，ex6",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "地下城ex5攻略，同理ex6也是",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "jjc",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "jjc攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "pjjc",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "pjjc攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "探险",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "探险刷图推荐",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "三角洲日报",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "每日三角洲报告",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "今日老婆",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "随机抽取群友作为老婆，当天已经抽取过回复相同老婆",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "地下城，sp地下城，特别地下城",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "特别地下城攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "表情包",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "可以查看头像表情包具体使用方法",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "深域",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "深域攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "次元，次元断层",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "次元断层攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "萌新攻略",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "萌新攻略",
+      "image": "./卡池/ue.png"
+    },
+    {
+      "name": "斗技场",
+      "start": "2025/08/29 05:00",
+      "end": "2025/09/13 04:59",
+      "shouldShow": true,
+      "advice": "斗技场攻略",
+      "image": "./卡池/ue.png"
+    }
+  ],
+  "showModal": false,
+  "modalImage": ""
+};
+
+// 导出配置数据
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = configData;
+} else {
+    window.configData = configData;
+}
