d077d0bc128b942e476291e9ec67464a0ea695fc
faiz555 <1837757414@qq.com>
2025-09-22 15:02:17 +0800

    10月上半刊

--- ghz-config.js
+++ ghz-config.js
@@ -10,22 +10,20 @@ const GHZconfigData = {
   ],
   "activeTab": 0,
   "d1Images": [
-    "./工会战/d1/d1-1.jpg",
     "./工会战/d1/d1-2.jpg",
-    "./工会战/d1/d1-3.jpg"
+    "./工会战/d1/d1.jpg"
   ],
   "d2Images": [
-    "./工会战/d2/d2-1.jpg",
+    "./工会战/d2/d2-1.png",
     "./工会战/d2/d2-2.jpg",
     "./工会战/d2/d2-3.jpg",
-    "./工会战/d2/d2-4.jpg"
+    "./工会战/d2/d2-4.jpg",
+    "./工会战/d2/d2-4a.jpg"
   ],
   "d3Images": [
     "./工会战/d3/d3-1.jpg",
-    "./工会战/d3/d3-1a.jpg",
     "./工会战/d3/d3-2.jpg",
-    "./工会战/d3/d3-3.jpg",
-    "./工会战/d3/d3-3a.jpg"
+    "./工会战/d3/d3-3.jpg"
   ],
   "d4Images": [
     "./工会战/d4/d4-1.jpg",
@@ -35,17 +33,17 @@ const GHZconfigData = {
   "d5Images": [
     "./工会战/d5/d5-1.jpg",
     "./工会战/d5/d5-2.jpg",
-    "./工会战/d5/d5-3.jpg",
-    "./工会战/d5/d5-4.jpg"
+    "./工会战/d5/d5-3.jpg"
   ],
   "d6Images": [
-    "./工会战/set刀/1.png",
-    "./工会战/set刀/2.png",
-    "./工会战/set刀/69752EACF1CBE43623B3B0FF71DFC065.jpg",
-    "./工会战/set刀/78D9C0D5FAC6697717FC509FCA95AA02.jpg",
-    "./工会战/set刀/A7995DD3567610B9E93819F6F0D1BF27.jpg",
-    "./工会战/set刀/B134391126AFF3A86CE960EAD6DF39BB.jpg",
-    "./工会战/set刀/E8191FD422523BEB63966DA403757086.jpg"
+    "./工会战/set刀/猫abc.png",
+    "./工会战/set刀/猫d1.png",
+    "./工会战/set刀/猫d2.png",
+    "./工会战/set刀/猫d3.png",
+    "./工会战/set刀/猫d4.png",
+    "./工会战/set刀/猫d5.png",
+    "./工会战/set刀/猫d6.png",
+    "./工会战/set刀/猫d7.png"
   ],
   "showBigImg": false,
   "bigImgSrc": ""
