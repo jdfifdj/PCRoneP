a3ed46cf1f3e82c62d0dcb686d297e1e8b9f3303
faiz555 <1837757414@qq.com>
2026-08-26 15:08:26 +0800

    26-8d

--- ghz-config-1.js
+++ ghz-config-1.js
@@ -10,42 +10,60 @@ const GHZconfigData = {
   ],
   "activeTab": 0,
   "d1Images": [
-    "./工会战/d1/4130b4cc7049df93ced7bed4754a74377200759 (1).png",
-    "./工会战/d1/76443fba5eda2d0f7aec8477ab9f1e117200759.png",
-    "./工会战/d1/QQ截图20260727143833.png"
+    "./工会战/d1/1447a279e1c04321cf5c834cf5c025fb485486381.png",
+    "./工会战/d1/1f937dce46ecd5348c2fd3e7838b9900485486381.png",
+    "./工会战/d1/60282cc0a9895cd89b3304e8984bda6e485486381.png",
+    "./工会战/d1/c6220ae6ff138b75a51e0753277f6595485486381.png",
+    "./工会战/d1/QQ20260826-150043.png",
+    "./工会战/d1/QQ20260826-150059.png"
   ],
   "d2Images": [
-    "./工会战/d2/15ca92b24c543d77a0a8af5b797d61b07200759.png",
-    "./工会战/d2/4550769c51dcee5170959b1031ec5b357200759.png",
-    "./工会战/d2/84d22ef79d92db273c0de8df802264ad7200759.png",
-    "./工会战/d2/bd8412ce3062c1f922a45d28b758f7037200759.png",
-    "./工会战/d2/f21e5e6961cf02e5b6caa98f705b6b1d7200759.png",
-    "./工会战/d2/QQ20260727-143845.png"
+    "./工会战/d2/803283e2cca95aa93901303cf5c9ad62485486381.png",
+    "./工会战/d2/QQ20260826-150110.png"
   ],
   "d3Images": [
-    "./工会战/d3/94a228ad3140be1928d01ead851743b67200759.png",
-    "./工会战/d3/QQ20260727-143854.png"
+    "./工会战/d3/ae51a7d96ab302e9393354e028ebb1f5485486381.png",
+    "./工会战/d3/QQ20260826-150139.png"
   ],
   "d4Images": [
-    "./工会战/d4/27ad35a62c0edc7f05cefcdc840f543c7200759.png",
-    "./工会战/d4/b104216f9dce8028d99f10d8b4e20a657200759.png",
-    "./工会战/d4/d4ae4ea55ece2a14efd267a9a98850ce7200759.png",
-    "./工会战/d4/QQ20260727-143906.png"
+    "./工会战/d4/20e67f6bda9adbc782e06ff21e554402485486381.png",
+    "./工会战/d4/QQ20260826-150155.png",
+    "./工会战/d4/QQ20260826-150211.png"
   ],
   "d5Images": [
-    "./工会战/d5/32544c14d1e2d22af9f2b4c0748521fd7200759.png",
-    "./工会战/d5/69597da4194d4111d8d0a881467e8f197200759.png",
-    "./工会战/d5/c721f1393591b7582e04650d8037b7237200759.png",
-    "./工会战/d5/e6c4a7d3f1350ca115f252038438806d7200759.png",
-    "./工会战/d5/QQ20260727-143914.png"
+    "./工会战/d5/7e4ca964e5bccf89d4cca796fdc3272f485486381.png",
+    "./工会战/d5/815eda5e95f132ce3accd982b5ec709a485486381.png",
+    "./工会战/d5/QQ20260826-150221.png"
   ],
   "d6Images": [
+    "./工会战/set刀/ev1.png",
+    "./工会战/set刀/ev2.png",
+    "./工会战/set刀/ev3.png",
+    "./工会战/set刀/ev4.png",
     "./工会战/set刀/hw1.png",
     "./工会战/set刀/hw2.png",
     "./工会战/set刀/hw3.png",
+    "./工会战/set刀/hw4.png",
+    "./工会战/set刀/hw5.png",
+    "./工会战/set刀/hw6.png",
     "./工会战/set刀/mx1.png",
     "./工会战/set刀/mx2.png",
-    "./工会战/set刀/mx3.png"
+    "./工会战/set刀/mx3.png",
+    "./工会战/set刀/mx4.png",
+    "./工会战/set刀/nc1.png",
+    "./工会战/set刀/nc2.png",
+    "./工会战/set刀/nc3.png",
+    "./工会战/set刀/nc4.png",
+    "./工会战/set刀/nc5.png",
+    "./工会战/set刀/nc6.png",
+    "./工会战/set刀/nc7.png",
+    "./工会战/set刀/nc8.png",
+    "./工会战/set刀/nc9.png",
+    "./工会战/set刀/wjm1.png",
+    "./工会战/set刀/wjm2.png",
+    "./工会战/set刀/wjm3.png",
+    "./工会战/set刀/wjm4.png",
+    "./工会战/set刀/wjm5.png"
   ],
   "showBigImg": false,
   "bigImgSrc": "",
@@ -57,32 +75,32 @@ const GHZconfigData = {
     },
     {
       "name": "玩具喵Official[自动-简单set刀]",
-      "url": "",
+      "url": "https://www.bilibili.com/video/BV1AMgF6PEHL",
       "cover": "./工会战/wjm.jpg"
     },
     {
       "name": "墨夕困困想睡觉[自动-简单set刀]",
-      "url": "https://www.bilibili.com/video/BV15t3K6BEAg/?spm_id_from=333.337.search-card.all.click&vd_source=8f074b7d90e0b4646126907ed781bafd",
+      "url": "https://www.bilibili.com/video/BV1D4hG6wENR",
       "cover": "./工会战/mx.jpg"
     },
     {
       "name": "玖璃梦_花舞组[自动-简单set刀]",
-      "url": "https://www.bilibili.com/opus/1226687327456198662?spm_id_from=333.1387.0.0",
+      "url": "https://www.bilibili.com/video/BV18Ch36hEwd",
       "cover": "./工会战/花舞.jpg"
     },
     {
       "name": "这是一个好记的昵称[自动-简单set刀]",
-      "url": "https://www.bilibili.com/video/BV1qaKp63EK6",
+      "url": "https://www.bilibili.com/video/BV1dqhV6VE1e",
       "cover": "./工会战/nc.jpg"
     },
     {
       "name": "-Evexy-[自动&手动-简单set刀]",
-      "url": "https://www.bilibili.com/opus/1216322931414532137?spm_id_from=333.1387.0.0",
+      "url": "https://www.bilibili.com/video/BV19Jh56yEog",
       "cover": "./工会战/evexy.jpg"
     },
     {
       "name": "浮城墟烟[简单set刀]",
-      "url": "https://space.bilibili.com/485486381?spm_id_from=333.788.upinfo.detail.click",
+      "url": "https://www.bilibili.com/video/BV1zN846SERo",
       "cover": "./工会战/浮.jpg"
     }
   ]
